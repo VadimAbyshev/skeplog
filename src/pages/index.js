8b2db502fe3835dc5ced73803b41745f0ef49bcc
VadimAbyshev <boss.abyshev@gmail.com>
2023-10-26 23:50:39 +0500

    акк

--- src/pages/index.js
+++ src/pages/index.js
@@ -136,3 +136,45 @@ burgerButton.addEventListener('click', openBurgerMenu)
 burgerButto.addEventListener('click', openBurgerMenu)
 
 closeBurgerButtton.addEventListener('click', closeBurgerMenu)
+
+
+// function getTimeRemaining(endtime) {
+//   var t = Date.parse(endtime) - Date.parse(new Date());
+//   var seconds = Math.floor((t / 1000) % 60);
+//   var minutes = Math.floor((t / 1000 / 60) % 60);
+//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  
+//   return {
+//     'total': t,
+//     'hours': hours,
+//     'minutes': minutes,
+//     'seconds': seconds
+//   };
+// }
+ 
+// function initializeClock(id, endtime) {
+//   var clock = document.getElementById(id);
+
+//   var hoursSpan = clock.querySelector('.hours');
+//   var minutesSpan = clock.querySelector('.minutes');
+//   var secondsSpan = clock.querySelector('.seconds');
+ 
+//   function updateClock() {
+//     var t = getTimeRemaining(endtime);
+ 
+//     daysSpan.innerHTML = t.days;
+//     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+//     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+//     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+ 
+//     if (t.total <= 0) {
+//       clearInterval(timeinterval);
+//     }
+//   }
+ 
+//   updateClock();
+//   var timeinterval = setInterval(updateClock, 1000);
+// }
+ 
+// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+// initializeClock('countdown', deadline);
